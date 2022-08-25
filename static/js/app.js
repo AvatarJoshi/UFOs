6e8aae491df79b39e7cb363ed0ab25e6e3c0c6a2
Avatar Joshi <joshi.avatar@gmail.com>
2022-08-25 10:55:17 -0700

    Need to figure out table headers still.

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+// Import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
@@ -17,8 +23,8 @@ function buildTable(data) {
       );
     });
   }
-
-  function handleClick() {
+  
+function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
@@ -29,16 +35,16 @@ function buildTable(data) {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
       filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
   
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
     buildTable(filteredData);
-  };
-
+  }
+  
   // Attach an event to listen for the form button
   d3.selectAll("#filter-btn").on("click", handleClick);
-
+  
   // Build the table when the page loads
   buildTable(tableData);
